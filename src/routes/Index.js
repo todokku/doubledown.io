ce55e3a2008109d2fb61b8a5c1104955a595646e
kyledkearney <kyledkearney@gmail.com>
2020-05-14 13:43:26 -0600

    setup pages and declared imports in router

---
+++ src/routes/Index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import Leaderboard from '../pages/Leaderboard'
+import Login from '../pages/login'
+import Play from '../pages/play'
+import Tutorial from '../pages/tutorial' 
