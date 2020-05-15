a9b4b6e51ab0a341d5997dfd7ebce5e91144ac14
kyledkearney <kyledkearney@gmail.com>
2020-05-14 19:50:00 -0600

    removed routing / ran npm eject / updated index.js location

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
-import { Router } from 'react-router-dom';
-import history from '../services/history';
-import Routes from '../routes/Index';
+import React from 'react';
+import logo from '../logo.svg';
+import '../css/App.css';
+
 function App() {
   return (
-    <Router history={history}>
-      <Routes />
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
-export default App
+export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './components/App';
-import Home from './components/Home'
-import * as serviceWorker from './serviceWorker';
+import App from '../components/App';
+import Home from '../components/Home'
+import * as serviceWorker from '../serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
