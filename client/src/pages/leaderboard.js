8e0fb462619910525f43aab315626cba048f38d9
kyledkearney <kyledkearney@gmail.com>
2020-05-15 22:04:29 -0600

    restructered for express server / setup package.json

---
+++ client/src/pages/leaderboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Leaderboard extends Component {
+render () {
+  return (
+    <>
+      <h1>Leaderboard</h1>
+    </>
+  )
+
+}
+
+}
---
+++ client/src/pages/mainMenu.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Menu from '../components/Menu'
+
+export default class MainMenu extends Component {
+render () {
+  return (
+    <>
+      <Menu/>
+    </>
+  )
+
+}
+
+}
