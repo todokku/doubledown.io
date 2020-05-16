41275b9eebd9ad2fca27fd6bcb003fb0652a867e
kyledkearney <kyledkearney@gmail.com>
2020-05-15 19:20:07 -0600

    added title

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,6 +7,7 @@ render () {
   return(
     <Row className='main-menu-row'>
       <Col xl={3} className='align-content-center justify-content-center main-menu-col'>
+        <h1>doubledown.io</h1>
         <h3 className='main-menu-item'><a href="/play">Play</a></h3>
         <h3 className='main-menu-item'><a href="/login">Login</a></h3>
         <h3 className='main-menu-item'><a href="/tutorial">Tutorial</a></h3>
